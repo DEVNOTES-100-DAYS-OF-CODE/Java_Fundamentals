24606b1f2ac10636b9d9126e8f246e9bce89d2eb
Jessica Dosseh <jessica.dosseh@yahoo.ca>
2020-08-29 13:36:11 -0400

    Hello world

---
+++ command_line_app/hello_world/Main.java
@@ -0,0 +1,12 @@
+package command_line_app.hello_world;
+
+public class Main {
+  public static void main(String[] args) {
+    // write your code here
+    // System class
+    // . operator
+    // out filed
+    // println method
+    System.out.println("Hello world");
+  }
+}
